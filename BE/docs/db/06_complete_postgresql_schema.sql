9f3443728944879041b4699cb0788d77f2c1313c
KUCSEPotato <a23493307@gmail.com>
2026-05-06 11:41:32 +0900

    db: add max_members column to projects table in complete schema

--- BE/docs/db/06_complete_postgresql_schema.sql
+++ BE/docs/db/06_complete_postgresql_schema.sql
@@ -6,8 +6,10 @@
 -- Execution order:
 -- 1. 04_postgresql_schema_with_idea_tech_stack_hashtags_backfill.sql
 -- 2. 05_add_todo_assignments_stage_templates.sql
+-- 3. 07_add_max_members_to_projects.sql
 --
 -- Use `psql` so relative includes resolve from this file's directory.
 
 \ir 04_postgresql_schema_with_idea_tech_stack_hashtags_backfill.sql
 \ir 05_add_todo_assignments_stage_templates.sql
+\ir 07_add_max_members_to_projects.sql
