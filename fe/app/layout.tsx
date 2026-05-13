379b9f351470104e5b482c96aa32669ef0148981
한은규 <han-eungyu@MacBook-Air-138.local>
2026-05-13 14:22:40 +0900

    fix: 회고페이지 다크모드 수정

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import AuthSessionNotifier from "../components/AuthSessionNotifier";
@@ -45,7 +46,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
+        <Script
+          id="devory-theme-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{ __html: themeInitScript }}
+        />
         <AppFeedbackProvider>
           <AuthSessionNotifier />
           <Navbar />
