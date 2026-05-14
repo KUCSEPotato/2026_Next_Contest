feb6d4ad51e2aa0bcdcfd0632cab269e855573a9
GBEdge01 <gooddino3@naver.com>
2026-05-14 23:34:43 +0900

    fix: 관리자 수동 승인 버그 수정

---
+++ BE/docs/db/36_add_purchase_notification_types.sql
@@ -0,0 +1,4 @@
+ALTER TYPE notification_type ADD VALUE IF NOT EXISTS 'coin_purchase_approved';
+ALTER TYPE notification_type ADD VALUE IF NOT EXISTS 'coin_purchase_rejected';
+ALTER TYPE notification_type ADD VALUE IF NOT EXISTS 'entitlement_purchase_approved';
+ALTER TYPE notification_type ADD VALUE IF NOT EXISTS 'entitlement_purchase_rejected';
